b048fe845248690e108c07c4d069011d0eeda47b
admin <krivalcevich@gmail.com>
2022-10-18 19:36:04 +0300

    HW5+Css

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -4,12 +4,16 @@ import s from "../h5-rrd-v6/Heade.module.css"
 
 function Header() {
     return (
+
         <div className={s.container}>
            <div><NavLink to='/pre-junior'>PreJun</NavLink></div>
            <div><NavLink to='/junior'>Jun</NavLink></div>
            <div><NavLink to='/junior-plus'>Jun+</NavLink></div>
+            <div className={s.block}></div>
 
         </div>
+
+
     )
 }
 
